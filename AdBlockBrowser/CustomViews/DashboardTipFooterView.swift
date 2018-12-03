9c1891ae921cee44c0a53efe4aef895a6eab4f97
Andy Shephard <a.shephard@eyeo.com>
2018-12-03 11:21:46 +0000

    eyeo/auxiliary/issue#206 - Fixed issue with TIP label layout clipping  on dashboard view

--- AdBlockBrowser/CustomViews/DashboardTipFooterView.swift
+++ AdBlockBrowser/CustomViews/DashboardTipFooterView.swift
@@ -19,9 +19,17 @@ import UIKit
 
 final class DashboardTipReusableView: UICollectionReusableView {
     @IBOutlet weak var tipLabel: UILabel?
+    @IBOutlet weak var tipViewWidth: NSLayoutConstraint!
+    @IBOutlet weak var tipLabelSpacing: NSLayoutConstraint!
 
     override func awakeFromNib() {
         super.awakeFromNib()
         tipLabel?.text = localize("dashboard_tip_text", comment: "Dashboard tip footer text")
+
+        // reservedWidthTotal being the width of |tipView|, plus the spacing between |tipView|
+        // and |tipLabel|, plus 16px for the margins (32px either side).
+        let reservedWidthTotal = tipViewWidth.constant + tipLabelSpacing.constant + 64.0
+        let maxTipLabelWidth = self.frame.size.width - reservedWidthTotal
+        tipLabel?.preferredMaxLayoutWidth = maxTipLabelWidth
     }
 }
